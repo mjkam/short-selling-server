66cea54d8e391b72a041d6ba4563568b66094430
mjkam <mjkam@naver.com>
2023-01-14 20:33:51 +0900

    refactor(krxapi): change test package name

--- src/test/java/com/example/demo/cron/KRXExternalApiTests.java
+++ src/test/java/com/example/demo/cron/KRXExternalApiTests.java
@@ -3,22 +3,12 @@ package com.example.demo.scheduler;
 import com.example.demo.TimeUtils;
 import com.example.demo.cron.KRXApi;
 import com.example.demo.cron.KRXStockRecord;
-import com.example.demo.domain.Company;
-import com.example.demo.domain.StockRecord;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
-import org.springframework.http.*;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
-import org.springframework.web.client.RestTemplate;
 
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class KRXExternalApiTests {
 
@@ -33,6 +23,4 @@ public class KRXExternalApiTests {
         //then
         assertThat(result.size()).isGreaterThan(0);
     }
-
-
 }
