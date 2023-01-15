f1d323f13a956bf0863048acc93cfa83f9c160c9
mjkam <mjkam@naver.com>
2023-01-15 12:23:37 +0900

    refactor(cron): remove unnecessary exception

--- src/main/java/com/example/demo/cron/StockRecordsSaver.java
+++ src/main/java/com/example/demo/cron/StockRecordsSaver.java
@@ -12,7 +12,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.text.ParseException;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
