b8627cec0ad51093ecb42f4c114af3d1ad8bb781
mjkam <mjkam@naver.com>
2023-01-19 22:23:33 +0900

    refactor(resttemplate): delete commented code

--- src/main/java/com/example/demo/config/RestTemplateConfiguration.java
+++ src/main/java/com/example/demo/config/RestTemplateConfiguration.java
@@ -16,30 +16,4 @@ public class RestTemplateConfiguration {
                 .setReadTimeout(Duration.ofSeconds(5L))
                 .build();
     }
-
-//    @Bean
-//    public ObjectMapper objectMapper() {
-//        return new ObjectMapper();
-//    }
-//
-//    static class RestTemplateResponseErrorHandler implements ResponseErrorHandler {
-//
-//        @Override
-//        public boolean hasError(ClientHttpResponse response) throws IOException {
-//            System.out.println("AAAAAAAAAAAAAAAAAAAAA");
-//            return !response.getStatusCode().equals(HttpStatus.OK);
-//        }
-//
-//        @Override
-//        public void handleError(ClientHttpResponse response) throws IOException {
-//            System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@");
-//            throw new HttpServerErrorException(response.getStatusCode(), readBody(response));
-//        }
-//
-//        private String readBody(ClientHttpResponse response) throws IOException {
-//            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(response.getBody()));
-//            return bufferedReader.lines().collect(Collectors.joining("\n"));
-//        }
-//    }
-
 }
