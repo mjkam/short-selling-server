4e57577e77804ab470038c36de40710ef81fb697
mjkam <mjkam@naver.com>
2023-01-19 22:37:59 +0900

    refactor(test): rename test method

--- src/test/java/com/example/demo/cron/KRXApiExceptionTests.java
+++ src/test/java/com/example/demo/cron/KRXApiExceptionTests.java
@@ -5,6 +5,7 @@ import com.example.demo.config.RestTemplateConfiguration;
 import com.example.demo.domain.MarketType;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.client.RestClientTest;
@@ -22,10 +23,13 @@ public class KRXApiExceptionTests {
     private MockRestServiceServer mockRestServiceServer;
 
     @Test
-    void test() {
+    @DisplayName("KRX API 에서 Json 이 아닌 값을 리턴하면 Exception 발생")
+    void throwExceptionWhenKRXApiReturnNotJson() {
+        //given
         mockRestServiceServer.expect(MockRestRequestMatchers.requestTo("https://data.krx.co.kr/comm/bldAttendant/getJsonData.cmd"))
                 .andRespond(MockRestResponseCreators.withSuccess("<html></html>", MediaType.TEXT_HTML));
 
+        //when then
         Assertions.assertThatThrownBy(
                 () -> krxApi.getStockRecordsAt(TimeUtils.localDate("2022-10-12"), MarketType.KOSPI))
                 .isInstanceOf(KRXApiResponseException.class);
