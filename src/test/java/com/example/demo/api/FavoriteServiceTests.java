ef8329592792ee9bd9e180339b301b405cda6030
mjkam <mjkam@naver.com>
2023-01-22 14:07:28 +0900

    refactor(test): remove unnecessary import

--- src/test/java/com/example/demo/api/FavoriteServiceTests.java
+++ src/test/java/com/example/demo/api/FavoriteServiceTests.java
@@ -8,8 +8,6 @@ import com.example.demo.domain.MarketType;
 import com.example.demo.repository.CompanyRepository;
 import com.example.demo.repository.FavoriteRecordRepository;
 import com.example.demo.service.FavoriteService;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -17,7 +15,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @SpringBootTest
 public class FavoriteServiceTests {
